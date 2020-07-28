f5979b3c5425e41dae20d6a94c3383d90ba038a9
alvesph <alvesph1@gmail.com>
2020-07-28 11:45:20 -0400

    Update links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import Menu from './componentes/Menu'
-//import dadosIniciais from '.data/dados_iniciais.json';
+import {  } from "module";
+import BannerMain from './componentes/BannerMain';
+import Carousel from './componentes/Carousel';
+import Footer from './componentes/Footer';
+import dadosIniciais from './data/dados_iniciais.json';
 
 function App() {
   return (
-    <div>
+    <div style={{ background: "#141414" }}>
       <Menu />
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é esse fenômeno infantil? Descobrindo por que meu filho gosta tanto desse desenho!"}
+      />
+      <Carousel 
+        iggnoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
 
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[4]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[5]}
+      />      
+
+      <Footer />
     </div>
   );
 }
