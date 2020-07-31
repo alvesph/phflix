322734f513217f638f2f9f281ba39c7378634b31
alvesph <alvesph1@gmail.com>
2020-07-31 11:37:43 -0400

    Iniciando o Stats

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../componentes/PageDefault';
 import { Link } from 'react-router-dom';
 
 function CadastroCategoria() {
+    const [nomeDaCategoria, setNomeDaCategoria] = useState('Valor Inicial');
+
+    
     return (
     <PageDefault> 
-        <h1>Cadastro de Categoria</h1>
+        <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
+
         <form>
             <label>
                 Nome da Categoria:
                 <input
                     type="text"
+                    value={nomeDaCategoria}
+                    onChange={function quandoMuda(infosDoEvento) {
+                    setNomeDaCategoria(infosDoEvento.target.value)
+                    }}
                 />
             </label>
 
@@ -18,7 +26,6 @@ function CadastroCategoria() {
                 Cadastrar
             </button>
         </form>
-
 
         <Link to="/">
             Ir para home
